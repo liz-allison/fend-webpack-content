d00e784d21dbdc86d17812c3e78d27d4d4710dcf
liz-allison <eeallison@gmail.com>
2021-03-08 20:11:36 -0800

    3/08 at 8:11PM

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
 const path = require("path")
 const webpack = require("webpack")
+
 module.exports = {
-    
+    entry: './src/client/index.js',
+    mode: 'none'
+}
+
+module: {
+    rules: [
+            {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+            }
+    ]
 }
